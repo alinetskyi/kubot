3567b4d07799c89bf58cc03a2985adea1000cd9b
alinetskyi <artem.linetskyi@gmail.conf>
2018-09-21 17:25:41 +0300

    Fixed database sql injections vulnarability

--- lib/kubot/db.rb
+++ lib/kubot/db.rb
@@ -38,19 +38,19 @@ module Kubot
     end
 
     def select_team_by_name(team_name)
-      self.execute("SELECT * FROM teams WHERE team_name = '#{team_name}'") do |row|
+      self.execute("SELECT * FROM teams WHERE team_name = (?)",[team_name]) do |row|
         return row
       end
     end
 
     def select_team_by_id(team_id)
-      self.execute("SELECT * FROM teams WHERE team_id = '#{team_id}'") do |row|
+      self.execute("SELECT * FROM teams WHERE team_id = (?)",[team_id]) do |row|
         return row
       end
     end
 
     def get_team_bot_token(team_id)
-      self.execute("SELECT bot_access_token FROM teams WHERE team_id = '#{team_id}'") do |row| 
+      self.execute("SELECT bot_access_token FROM teams WHERE team_id = (?)",[team_id]) do |row| 
         return row
       end
     end
@@ -64,31 +64,31 @@ module Kubot
     end
 
     def get_team_name(team_id)
-      self.execute("Select team_name FROM teams WHERE team_id = '#{team_id}'") do |row|
+      self.execute("Select team_name FROM teams WHERE team_id = (?)",[team_id]) do |row|
         return row
       end
     end
 
     def get_team_token(team_id)
-      self.execute("SELECT access_token FROM teams WHERE team_id = '#{team_id}'") do |row| 
+      self.execute("SELECT access_token FROM teams WHERE team_id = (?)",[team_id]) do |row| 
         return row
       end
     end
 
     def get_bot_id(team_id)
-      self.execute("SELECT bot_id FROM teams WHERE team_id = '#{team_id}'") do |row|
+      self.execute("SELECT bot_id FROM teams WHERE team_id = (?)",[team_id]) do |row|
         return row
       end
     end
 
     def select_support_channel(ask_channel)
-      self.execute("SELECT support_channel FROM channels WHERE ask_channel = '#{ask_channel}'") do |row|
+      self.execute("SELECT support_channel FROM channels WHERE ask_channel = (?)",[ask_channel]) do |row|
         return row
       end
     end
 
     def select_ask_channel(support_channel)
-      self.execute("SELECT ask_channel FROM channels WHERE support_channel = '#{support_channel}'") do |row|
+      self.execute("SELECT ask_channel FROM channels WHERE support_channel = (?)",[support_channel]) do |row|
         return row
       end
     end
@@ -98,12 +98,12 @@ module Kubot
       end
     end
     def get_ask_bot_token(ask_channel)
-      self.execute("SELECT ask_channel_bot_token FROM channels WHERE ask_channel = '#{ask_channel}'") do |row|
+      self.execute("SELECT ask_channel_bot_token FROM channels WHERE ask_channel = (?)",[ask_channel]) do |row|
         return row
       end
     end
     def get_support_bot_token(support_channel)
-      self.execute("SELECT support_channel_bot_token FROM channels WHERE support_channel = '#{support_channel}'") do |row|
+      self.execute("SELECT support_channel_bot_token FROM channels WHERE support_channel = (?)",[support_channel]) do |row|
         return row
       end
     end
